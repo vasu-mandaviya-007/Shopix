f5489fe07886e4487575942d1125feda666b12ea
vasu-mandaviya-007 <vasumandaviya@gmail.com>
2026-04-08 04:33:16 +0530

    Debug Fix

--- backend/ecomm/settings.py
+++ backend/ecomm/settings.py
@@ -11,8 +11,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config("SECRET_KEY")
 
-# DEBUG = config("DEBUG", default=False, cast=bool)
-DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=bool)
+# DEBUG = True
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="", cast=Csv())
 
